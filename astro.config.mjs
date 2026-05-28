cca28173a30ae714a845bba0f3fcc571b367d9d9
Yakup <yakupdgrr44@gmail.com>
2026-05-29 02:14:37 +0300

    speed test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  build: {
+    // 4kb altındaki tüm CSS dosyalarını otomatik olarak HTML içine gömer
+    inlineStylesheets: 'always', 
+  },
 });
